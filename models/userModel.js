2b380c5a4b536bc00f88318629f2cd6ed73cfc3b
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2024-05-18 23:49:10 +0600

    [DONE] user model complete

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
