8011a4548af83082a31b2a2db909e6f88d12aba1
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2024-05-22 12:23:14 +0600

    [DONE] login api integratrion completed

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -15,12 +16,12 @@ router.post("/register", async (req, res) => {
         success: false,
       });
     }
-    // Hash password
+    //! Hash password
     const saltRound = await bcrypt.genSalt(12);
     const hashPassword = await bcrypt.hash(req.body.password, saltRound);
     req.body.password = hashPassword;
 
-    // create new user
+    //! create new user
     const newUser = new User(req.body);
     await newUser.save();
     res.send({
@@ -32,6 +33,49 @@ router.post("/register", async (req, res) => {
       message: error.message,
       data: error,
       success: false,
+    });
+  }
+});
+
+//! User Login API
+router.post("/login", async (req, res) => {
+  try {
+    //! Check the user is exist
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "User does not exist", success: false });
+    }
+
+    //! check password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(200).send({
+        message: "Invalid password",
+        success: false,
+      });
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+    res.send({
+      message: "User logged in successfully",
+      success: true,
+      data: token,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      success: false,
+      data: error,
     });
   }
 });
